a5b04f7b927dad21523b8cd8b8c7483a0470d58b
OdujokoD <odujoko.dml30@gmail.com>
2016-12-10 21:18:16 +0100

    Create stock activity

---
+++ app/src/main/java/com/example/agrofarmers/views/StockActivity.java
@@ -0,0 +1,15 @@
+package com.example.agrofarmers.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.agrofarmers.R;
+
+public class StockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock);
+    }
+}

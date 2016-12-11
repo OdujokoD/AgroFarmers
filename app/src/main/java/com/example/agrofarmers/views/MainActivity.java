6ded5aa744faff4a88c0d3af459a8a62dc04c963
OdujokoD <odujoko.dml30@gmail.com>
2016-12-11 16:02:19 +0100

    Add the basic fragments of the app

--- app/src/main/java/com/example/agrofarmers/views/MainActivity.java
+++ app/src/main/java/com/example/agrofarmers/views/MainActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.example.agrofarmers.R;
 
-public class StockActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stock);
+        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/example/agrofarmers/views/StockFragment.java
@@ -0,0 +1,45 @@
+package com.example.agrofarmers.views;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.example.agrofarmers.R;
+
+public class StockFragment extends Fragment {
+
+    private FrameLayout frameLayout;
+
+    public StockFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate layout for this fragment
+        frameLayout = (FrameLayout) inflater.inflate(R.layout.fragment_stock, container, false);
+
+
+        return frameLayout;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
